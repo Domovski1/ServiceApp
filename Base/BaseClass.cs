6d20eb2be87f2bb3f7fa6c2c87317bbb5fdc236b
Domovski1 <624273242@mail.ru>
2021-02-08 09:44:54 +0300

    Добавлена база данных

---
+++ Base/BaseClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTest.Base
+{
+    class BaseClass
+    {
+        public static DemoTestBaseEntities db = new DemoTestBaseEntities();
+    }
+}
