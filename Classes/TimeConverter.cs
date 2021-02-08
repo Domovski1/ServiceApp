ae38392fae20710dd817f9f32c7f5749552dabac
Domovski1 <624273242@mail.ru>
2021-02-08 20:11:24 +0300

    почистил код чуток

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
@@ -18,20 +14,13 @@ namespace DemoTest.Classes
                 return "информация отсутствует";
             }
 
-            int seconds = int.Parse(value.ToString());
+            var seconds = int.Parse(value.ToString());
 
-            if (seconds > 60 && seconds < 3600)
-            {
-                return (seconds / 60 + " минут");
-            }
-            else if (seconds > 3600 && seconds < 7200)
-            {
-                return (seconds / 3600 + " h " + (seconds / 60 - 60) + " m");
-            }
-            else if (seconds > 7200 && seconds < 14400)
-            {
-                return (seconds / 3600 + " Часа " + (seconds / 60 - 120) + " минут");
-            }
+            if (seconds > 60 && seconds < 3600) return (seconds / 60 + " минут");
+            
+            else if (seconds > 3600 && seconds < 7200) return (seconds / 3600 + " h " + (seconds / 60 - 60) + " m");
+            
+            else if (seconds > 7200 && seconds < 14400) return (seconds / 3600 + " Часа " + (seconds / 60 - 120) + " минут");
 
             return seconds;
         }
