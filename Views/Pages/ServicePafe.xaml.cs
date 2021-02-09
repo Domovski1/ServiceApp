ec68a7b448707ee9e7a99dd55210674b73d1c2d3
Domovski1 <624273242@mail.ru>
2021-02-10 00:55:20 +0300

    Добавлены ограничения на запись цифр, проверка длительности записи, наличия сервиса

--- Views/Pages/ServicePafe.xaml.cs
+++ Views/Pages/ServicePafe.xaml.cs
@@ -64,16 +64,47 @@ namespace DemoTest.Views.Pages
         void ShowServiceID()
         {
             if (service.ID != 0) LblId.Content = $"ID: {service.ID}";
+            else LblId.Content = "Новый сервис";
         }
 
-        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        private void BtnSave_Click(object sender, RoutedEventArgs e) 
         {
-            if (service.ID == 0)
+            try
             {
-                BaseClass.db.Service.Add(service);
+                var ServiceExist = BaseClass.db.Service.FirstOrDefault(s => s.Title == TxbTitle.Text);
+
+
+                if (service.ID == 0)
+                {
+                    if (ServiceExist != null) MessageBox.Show("Такой сервис существует!");
+                    BaseClass.db.Service.Add(service);
+                    BaseClass.db.SaveChanges();
+                    MessageBox.Show("Операция выполнена", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                } else
+                {
+                    BaseClass.db.SaveChanges();
+                    MessageBox.Show("Операция выполнена", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+
             }
-            BaseClass.db.SaveChanges();
-            MessageBox.Show("Операция выполнена", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Упс, что-то пошло не так", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        void CheckData()
+        {
+            int TimeLimit = int.Parse(TxbTime.Text);
+            if (TimeLimit > 14400)
+            {
+                BtnSave.IsEnabled = false;
+            }
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = "0123456789.".IndexOf(e.Text) < 0;
         }
     }
 }
