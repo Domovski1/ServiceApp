df6863974873091e1a8a3e8aa60025f3eb4b5064
Domovski1 <624273242@mail.ru>
2021-02-09 20:42:44 +0300

    Работа сортировки через switch.
    Не работает сортировка по скидке

--- Views/Pages/ServiceListPage.xaml.cs
+++ Views/Pages/ServiceListPage.xaml.cs
@@ -30,11 +30,63 @@ namespace DemoTest.Views.Pages
 
         void Clicked()
         {
-
-            //TextBlock textBlock = new TextBlock(new Run("This text will render"));
-            //textBlock.TextDecorations = TextDecorations.Strikethrough;
-
             TxbCost.TextDecorations = TextDecorations.Strikethrough;
+        }
+
+        private void CmbSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (CmbSearch.Text)
+            {
+                case "По убыванию":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.OrderByDescending(x => x.Cost));
+                    this.DataContext = null;
+                    this.DataContext = this;
+                    break;
+                case "По возрастанию":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.OrderBy(x => x.Cost));
+                    this.DataContext = null;
+                    this.DataContext = this;
+                    break;
+                case "Скидка 0-5%":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.Where(x => x.Discount < 5));
+                    this.DataContext = null;
+                    this.DataContext = this; ;
+                    break;
+                case "Скидка 5-15%":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.Where(x => x.Discount >= 5 && x.Discount < 15));
+                    this.DataContext = null;
+                    this.DataContext = this; ;
+                    break;
+                case "Скидка 15-30%":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.Where(x => x.Discount >= 15 && x.Discount < 30));
+                    this.DataContext = null;
+                    this.DataContext = this; ;
+                    break;
+                case "Скидка 30-70%":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.Where(x => x.Discount >= 30 && x.Discount < 70));
+                    this.DataContext = null;
+                    this.DataContext = this; ;
+                    break;
+                case "Скидка 70-100%":
+                    services = new ObservableCollection<Service>(BaseClass.db.Service.Where(x => x.Discount >= 70 && x.Discount <100));
+                    this.DataContext = null;
+                    this.DataContext = this; ;
+                    break;
+            }
+
+            //if (CmbSearch.Text == "По убыванию")
+            //{
+            //    //LWService.ItemsSource = BaseClass.db.Service.OrderByDescending(x => x.Cost).ToString();
+            //    services = new ObservableCollection<Service>(BaseClass.db.Service.OrderByDescending(x => x.Cost));
+            //    this.DataContext = null;
+            //    this.DataContext = this;
+            //} else if (CmbSearch.Text == "По возрастанию")
+            //{
+            //    //LWService.ItemsSource = BaseClass.db.Service.OrderBy(x => x.Cost).ToString();
+            //    services = new ObservableCollection<Service>(BaseClass.db.Service.OrderBy(x => x.Cost));
+            //    this.DataContext = null;
+            //    this.DataContext = this;
+            //}
         }
     }
 }
