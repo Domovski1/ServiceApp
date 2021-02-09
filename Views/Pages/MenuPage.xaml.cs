130c94ad358e7c80511629a8943804c54f02c5ed
Domovski1 <624273242@mail.ru>
2021-02-10 01:33:19 +0300

    Добавлена страница с меню

---
+++ Views/Pages/MenuPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+
+namespace DemoTest.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuPage.xaml
+    /// </summary>
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnToList_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ServiceListPage());
+        }
+
+        private void BtnToService_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ServicePafe(new Base.Service));
+        }
+    }
+}
